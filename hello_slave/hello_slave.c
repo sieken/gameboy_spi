c7de16e65fe31d8610ae620545bb1c977e737781
sieken <kdhenriksson@gmail.com>
2016-03-02 16:08:21 +0100

    ISR handshake implemented on GB

--- hello_slave/hello_slave.c
+++ hello_slave/hello_slave.c
@@ -10,19 +10,41 @@
 char message[19] = { 0 };
 UBYTE handshake = 0x00;
 
-#define TR_SIZE   19
+#define IFLAGS    *(volatile UBYTE *) 0xFF0F
 #define SC        *(volatile UBYTE *) 0xFF02
+#define SB        *(volatile UBYTE *) 0xFF01
+
 #define CR        0x0D
+#define TR_SIZE   19
+#define HANDSHAKE 0xAA
+
+/* All functions in advance */
+void receive (void);
+void debug_receive (void);
+void debug_send (void);
+void wait_handshake (void);
+void sio_isr (void);
+void setup_isr (void);
+
+
+
+void receive (void) {
+  receive_byte();
+  while (_io_status == IO_RECEIVING) {
+    /* Wait for IO completion */
+  }
+}
 
 void debug_receive (void) {
   printf("DEBUGGING receive_byte()\n");
+  receive();
   while (_io_status == IO_RECEIVING) {
     /* wait for io completion */
   }
   if (_io_status == IO_IDLE) {
-    printf("Received: %c\n",_io_in);
+    printf("Received: %x\n",_io_in);
   } else {
-    printf("Error: %c\n",_io_status);
+    printf("Error: %x\n",_io_status);
   }
 }
 
@@ -32,58 +54,90 @@ void debug_send (void) {
     /* wait for io completion */
   }
   if (_io_status == IO_IDLE) {
-    printf("Byte sent: %c\n");
+    printf("Byte sent");
   } else {
-    printf("Error: %c\n",_io_status);
+    printf("Error: %x\n",_io_status);
   }
 }
 
 /* handshake routine */
 void wait_handshake (void) {
-  while (1) {
-    _io_out = 0;
-    if (joypad() & J_A) {
-      waitpadup();
-      handshake = 0xAA;
-      _io_out = handshake;
-      receive_byte();
-      debug_receive();
-    }
-  }
+    /* THIS IS CRAP FOR NOW
+     UBYTE ok = 0x00;
+     UBYTE received;
+
+     _io_out = 0x00;
+
+     while (1) {
+     if (joypad() & J_A) {
+     waitpadup();
+     receive();
+     ok = 0x01;
+     _io_out = HANDSHAKE;
+     }
+
+     if (ok) {
+     receive();
+     received = (UBYTE)_io_in;
+     if (received == HANDSHAKE) {
+     printf("Handshake OK!: %x\n", received);
+     } else {
+     printf("Received: %x : %c\n", received, received);
+     }
+     _io_out = HANDSHAKE;
+     }
+     } */
+}
+
+void sio_isr (void) {
+  handshake = SB;
+  SB = 0xAA;
+  printf("INTERRUPTS FUNKAR!\n");
+  printf("RECEIVED: %x : %c\n", handshake, handshake);
+}
+
+void setup_isr (void) {
+  disable_interrupts();
+  IFLAGS = (UBYTE) 0x00;
+  add_SIO(sio_isr);
+  enable_interrupts();
+  set_interrupts(SIO_IFLAG);
 }
 
 int main (void) {
-  UBYTE ccount = 0;
-  UBYTE output = 0x00;
-  char input;
+//  UBYTE ccount = 0;
+//  UBYTE output = 0x00;
+//  char input;
+  printf("RECEIVING");
+  delay(1000);
+  setup_isr();
 
   /* don't start receiving until ready */
   printf("Incoming transmission\n");
-  wait_handshake();
   printf("...\n");
 
   /* main routine */
-  while (1) {
-    _io_out = output;      // write c to gb serial out buffer
-    receive_byte();
-
-    /* wait for receive done */
-    while (_io_status == IO_RECEIVING) {;}
-    input = ((char)_io_in);
-
-    /* check for carriage return */
-    if (input == (char)CR ) {
-      /* print & clear */
-      for (ccount = 0; ccount < TR_SIZE; ccount++) {
-        printf("%c",message[ccount]);
-        message[ccount] = 0;
-      }
-      printf("\n");
-      ccount = 0;
-    } else {
-      message[ccount] = input;
-    }
-    ccount++;
-  }
-  return 0;
+  while (1) {}
+//    _io_out = output;      // write c to gb serial out buffer
+//    receive_byte();
+//
+//    /* wait for receive done */
+//    while (_io_status == IO_RECEIVING) {;}
+//    input = ((char)_io_in);
+//
+//    /* check for carriage return */
+//    if (input == (char)CR ) {
+//      /* print & clear */
+//      for (ccount = 0; ccount < TR_SIZE; ccount++) {
+//        printf("%c",message[ccount]);
+//        message[ccount] = 0;
+//      }
+//      printf("\n");
+//      ccount = 0;
+//    } else {
+//      message[ccount] = input;
+//    }
+//    ccount++;
+//  }
+//  return 0;
 }
