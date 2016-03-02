e8ec81c3f0b40ee4c6d15fa3e4bea58f6e0ece39
sieken <kdhenriksson@gmail.com>
2016-03-02 06:32:24 +0100

    added comments and upped readability

--- main.c
+++ main.c
@@ -6,28 +6,36 @@
 #include <stdint.h>
 #include <pic32mx.h>
 
-#define SLEEP 2000000
 
-// uint8_t message[3] = {0x53,0x4F,0x53};
+uint8_t clrBuf;
 char message[] = {
   'S','o','c',0x0D,'l','i','s','t','e','r',' ','h','e','m',' ',0x0D,
   'u','r',' ','h','u','s','e',' ','h','a',' ','h','a',' ',0x0D,
   'l','o','l',',','t','r','o','d','d','e',' ','d','u',0x0D,
   'j','a','.',' ','*','F','i','s','!',' ',' ',' ',0x0D,
 };
-uint8_t clrBuf;
+
+
+#define MS_LENGTH   (sizeof(message)/sizeof(message[0]))
+#define SLEEP       2000000
 
 void spi_init (void);
 void send (char c);
 void sleep (void);
 
+/* SPI initialization routine */
 void spi_init (void) {
   SPI2CON = 0;
-  SPI2BRG = 0x1FF;
+  IEC(1) = 0;
+  IPC(7) = 0;
+  IFS(1) = 0;
+
+  SPI2BRG = 0x1FF;      // Set SCK ~78kHz
   SPI2STATCLR = 0x40;
-  SPI2CON = 0x8060;
+  SPI2CON = 0x8060;     // Set ON, CKE, MSTEN
 }
 
+/* send one character (byte) of data via SPI */
 void send (char c) {
   while(!(SPI2STAT & 0x08));
   SPI2BUF = c;
@@ -35,29 +43,30 @@ void send (char c) {
   clrBuf = SPI2BUF;
 }
 
+/* bad sleep function */
 void sleep (void) {
   int i;
-  for (i = 0; i < SLEEP; i++) {
-    /* do nothing */
-  }
+  for (i = 0; i < SLEEP; i++) {/* do nothing */}
 }
 
 int main (void) {
-  uint8_t i;
+  uint8_t ccount;
   char c;
 
+  /* initialize SPI & LED5 */
   spi_init();
   TRISF &= ~0x1;
   PORTF &= ~0x1;
 
+  /* main routine */
   while (1) {
-    c = message[i];
+    c = message[ccount];
     send (c);
     PORTF ^= 0x1;
     sleep();
-    i++;
-    if (i > 58)
-      i = 0;
+    ccount++;
+    if (ccount > MS_LENGTH)
+      ccount = 0;
   }
   return 0;
 }
