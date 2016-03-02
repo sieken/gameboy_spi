856611bdbc7bd6c96dac3da729eb65e34793b2bd
sieken <kdhenriksson@gmail.com>
2016-03-02 05:51:27 +0100

    added description and comments, and upped readability

--- hello_slave/hello_slave.c
+++ hello_slave/hello_slave.c
@@ -1,41 +1,54 @@
+/* hello_slave.c 
+   
+   a hello world type program that receives a byte of information
+   via the Game Boy's link cable, and displays it on screen. */
+
 #include <stdio.h>
 #include <gb/gb.h>
 
-#define SC  *(volatile UBYTE *) 0xFF02
-#define CR  0x0D
+char message[19] = { 0 };
 
-char transmission[19] = { 0 };
+#define TR_SIZE   (sizeof(message)/sizeof(message[]))
+#define SC        *(volatile UBYTE *) 0xFF02
+#define CR        0x0D
 
 void debug_receive (void) {
   if (_io_status == IO_IDLE) {
-    printf ("Received: %c\n",_io_in);
+    printf("Received: %c\n",_io_in);
   } else {
-    printf ("Error: %c\n",_io_status);
+    printf("Error: %c\n",_io_status);
   }
 }
 
 int main (void) {
-  UBYTE i = 0;
+  UBYTE ccount = 0;
   UBYTE c = 0xAA;
   char input;
+
+  /* don't start receiving until ready */
   printf("Incoming transmission...\n");
+  waitpad();
+  waitpadup()
 
+  /* main routine */
   while (1) {
-    _io_out = c;
+    _io_out = c;      // write c to gb serial out buffer
     receive_byte();
-    while (_io_status == IO_RECEIVING) {;}
 
+    /* wait for receive done */
+    while (_io_status == IO_RECEIVING) {;}
     input = (char) _io_in;
+
+    /* check for carriage return */
     if (input == (char)CR ) {
-      for (i = 0; i < 3; i++) { 
-        printf("%c",transmission[i]);
-      }
+      for (ccount = 0; ccount < TR_SIZE; ccount++) 
+        printf("%c",message[ccount]);
       printf("\n");
-      i = 0;
+      ccount = 0;
     } else { 
-      transmission[i] = input;
+      message[i] = input;
     }
-    i++;
+    ccount++;
   }
 
   return 0;
